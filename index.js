3987684c083d797d36ab731c08a487740f8bc047
yusufgwandi736-lab <yusufgwandi736@gmail.com>
2026-07-24 09:10:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require("http")
 http.createServer((req,res)=> res.end("Bot Running")).listen(process.env.PORT || 3000, ()=> console.log("Server started"))
 
 async function startBot(){
-const { state, saveCreds } = await useMultiFileAuthState("auth2")
+const { state, saveCreds } = await    useMultiFileAuthState("auth3")
 const { version } = await fetchLatestBaileysVersion()
 console.log("Using WA Version:", version)
 
